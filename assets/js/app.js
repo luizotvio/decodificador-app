4058e94f561add24dc63e8c19fa8629911a5c4d4
luizotvio <luizotavio.03@outlook.com>
2024-08-11 14:24:45 -0300

    feat: finalizar lógica do app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,67 @@
+const contentText = document.querySelector('.container-input__text');
 const responseText = document.querySelector('.container-output__response');
 const titleResponse = document.querySelector('.container-output__title');
-const btnCripto = document.querySelector('.container-input__btn__cripto')
+const btnCripto = document.querySelector('.container-input__btn__cripto');
+const btnDescripto = document.querySelector('.container-input__btn__descripto');
 const btnCopy = document.getElementById('copy');
 
-// function criptografar(texto) {
-//     const chaves = {
-//         'a': 'ai',
-//         'e': 'enter',
-//         'i': 'imes',
-//         'o': 'ober',
-//         'u': 'ufat'
-//     }
-//     return chaves[texto]
-// }
+function criptografar(texto) {
+    let chaves = {
+        'e': 'enter',
+        'i': 'imes',
+        'a': 'ai',
+        'o': 'ober',
+        'u': 'ufat'
+    };
+    texto = texto.toLowerCase();
+
+    for(let chave in chaves) {
+        if(texto.includes(chave)) {
+            texto = texto.replaceAll(chave, chaves[chave])
+        }
+    }
+    return texto;
+}
 
 btnCripto.addEventListener('click', () => {
-    const contentText = document.querySelector('.container-input__text').value;
-    responseText.textContent = contentText;
+    const textoCripto = criptografar(contentText.value);
+    responseText.textContent = textoCripto;
     responseText.style.fontSize = '2rem';
     titleResponse.style.display = 'none';
     btnCopy.style.display = 'block';
+    contentText.value = '';
 })
 
-btnCopy.addEventListener('click', function(){
+function descriptografar(textoCripto) {
+    let chaves = {
+        'enter' : 'e',
+        'imes' : 'i',
+        'ai' : 'a',
+        'ober' : 'o',
+        'ufat' : 'u' 
+    };
+    textoCripto = textoCripto.toLowerCase();
+
+    for(let chave in chaves) {
+        if(textoCripto.includes(chave)) {
+            textoCripto = textoCripto.replaceAll(chave, chaves[chave])
+        }
+    }
+    return textoCripto;
+}
+
+btnDescripto.addEventListener('click', () => {
+    const textoDescripto = descriptografar(contentText.value);
+    responseText.textContent = textoDescripto;
+    responseText.style.fontSize = '2rem';
+    titleResponse.style.display = 'none';
+    btnCopy.style.display = 'block';
+    btnCopy.textContent = 'Copiar'
+    contentText.value = '';
+})
+
+btnCopy.addEventListener('click', function () {
     const responseText = document.querySelector('.container-output__response');
     navigator.clipboard.writeText(responseText.textContent);
+    btnCopy.textContent = 'Copiado!'
 })
